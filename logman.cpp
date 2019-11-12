328099b0b3cb2262260d77974a27d7aee50614b8
Prabdeep Dhaliwal <prabd@umich.edu>
2019-11-11 22:30:00 -0500

    Added Custom Comp for Entries
    TODO:
    - Sort master, create vector for index conversions
    - Create maps for keywords and categories
    - Create deque for recent searches
    - Everything else lol

--- logman.cpp
+++ logman.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <unordered_map>
 #include <iostream>
+#include <fstream>
 using std::cin;
 using std::cout;
 
@@ -32,6 +33,7 @@ struct Entry
 	: id{ i }, time{ ts }, cat{ c }, msg{ m } {}
 };
 
+
 // Helper function to convert from mm:dd:hh:mm:ss to a value
 uint64_t convert_time(std::string &ts)
 {
@@ -54,15 +56,53 @@ class stringPred
 	}
 };
 
+class EntryLess
+{
+	// Compares by timestamp, category, then ID
+	// Entry is less if timestamp is less, cat is less, entry id is less
+	bool operator()(const Entry* lhs, const Entry* rhs)
+	{
+		if(lhs->time < rhs-> time)
+		{
+			return true;
+		}
+		else if(lhs->time > rhs->time)
+		{
+			return false;
+		}
+		// If timestamps are equal
+		else
+		{
+			int val = strcasecmp(lhs->cat.c_str(), rhs->cat.c_str());
+			if(val < 0)
+			{
+				return true;
+			}
+			else if(val > 0)
+			{
+				return false;
+			}
+			// If cats are equal
+			else
+			{
+				return lhs->id < rhs->id;
+			}
+		}
+	}
+};
+
 class Logger
 {
+	
 	std::string logfile; // stores filename
+	std::vector<Entry*> master;
+	std::vector<int> id_to_index; // stores where an entryId can be found in master
 public:
 	
 	/*
-	 * THIS FUNCTION READS COMMAND LINE AND UPDATES THE PARAMETERS OF THE HUNT
+	 * THIS FUNCTION READS COMMAND LINE ARGS
 	 */
-	void get_options(int argc, char* argv[])
+	void read_cmd(int argc, char* argv[])
 	{
 		// If no -h or filename specified
 		if (argc != 2)
@@ -77,21 +117,52 @@ public:
 		}
 		// Else
 		logfile = cmd;
-	} //get_options
+	} //read_cmd
 
 	
 	/**
 	 * This function opens the file, processes it, and stores the entries in a vector
+	 * 
 	 */
 	void read_logfile()
 	{
-		
+		std::ifstream input;
+		input.open(logfile.c_str());
+
+		std::string ts, cat, msg;
+		int num = 0;
+		while(getline(input, ts, '|')){
+			std::getline(input, cat, '|');
+			std::getline(input, msg);
+			Entry* en = new Entry(num, convert_time(ts), cat, msg);
+			master.push_back(en);
+			++num;
+		}
+
+		// Print number of entries read
+		cout << master.size() << " entries read\n";
+
+		// Update
+	}
+
+	/**
+	 * Prints contents of master 
+	 */
+	void print_master()
+	{
+		for(int i = 0; i < int(master.size()); ++i)
+		{
+			cout << master[i]->id << " " << master[i]->time << " " << master[i]->cat << " " << master[i]->msg << "\n";
+		}
 	}
 };
 
 int main(int argc, char* argv[])
 {
-	std::string str = "99:88:77:66:55";
-	uint64_t time = convert_time(str);
-	cout << time;
+	std::ios_base::sync_with_stdio(false);
+
+	Logger log;
+	log.read_cmd(argc, argv);
+	log.read_logfile();
+	log.print_master();
 }
